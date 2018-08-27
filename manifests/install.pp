3d8f0f5f77d08b0a30047002b069c50add6548db
Sebastian Rakel <sebastian@devunit.eu>
2018-08-27 13:48:47 +0200

    Add python-yaml package

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,7 +12,19 @@ class borg::install {
     ensure   => $borg::package_ensure,
     provider => $real_package_provider,
   }
+   
   ## additional packages
+  if $facts['os']['family'] == 'Archlinux' {
+    $real_python_yaml_package_name = 'python-yaml'
+  } else {
+    $real_python_yaml_package_name = 'python3-yaml'
+  }
+
+  package{$real_python_yaml_package_name:
+    ensure   => $borg::package_ensure,
+    provider => $real_package_provider,
+  }
+  
   # centos: perl-autodie perl-Carp-Assert perl-DateTime perl-DB_File perl-File-Slurp perl-IO-Compress perl-IPC-Run
   # ... okay, List::Util is way to old...
 
