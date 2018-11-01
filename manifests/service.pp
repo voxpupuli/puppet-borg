cb042849f0e008f59207190a3e8f12ea246116b3
Tim Meusel <tim@bastelfreak.de>
2018-11-02 00:34:22 +0100

    cleanup epp() template

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,8 +6,10 @@ class borg::service {
   if $facts['systemd'] {
     systemd::unit_file{'borg-backup.service':
       content => epp("${module_name}/borg-backup.service.epp", {
-        'create_prometheus_metrics' => $borg::create_prometheus_metrics,
-        'restore_script_path'       => $borg::restore_script_path
+        'create_prometheus_metrics'              => $borg::create_prometheus_metrics,
+        'restore_script_path'                    => $borg::restore_script_path,
+        'use_upstream_reporter'                  => $borg::use_upstream_reporter,
+        'update_borg_restore_db_after_backuprun' => $borg::update_borg_restore_db_after_backuprun
         }
       ),
     }
