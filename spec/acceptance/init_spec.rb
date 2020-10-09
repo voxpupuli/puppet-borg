035b2bae948099147c708752c49f87163f321aae
Tim Meusel <tim@bastelfreak.de>
2020-10-09 09:15:43 +0200

    Add acceptance test to verify repo creation
    On some operating systems we setup an upstream borg repo to get the
    latest version. It's a good idea to validate that with a test.

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -21,6 +21,25 @@ describe 'borg' do
       it { is_expected.to be_running }
     end
   end
+  context 'with a backup server and default repositry setup' do
+    let(:pp) do
+      <<-PUPPET
+      class { 'borg':
+        backupserver => 'localhost',
+      }
+      PUPPET
+    end
+
+    it 'works idempotently with no errors' do
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe service('borg-backup.timer') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+  end
   context 'with a backup server and App:BorgRestore' do
     let(:pp) do
       <<-PUPPET
